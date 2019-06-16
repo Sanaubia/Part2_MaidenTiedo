be4864e4b19382cf68e75218be595446cde8ab31
Sanaubia <jprokkola@gmail.com>
2019-06-16 19:26:52 +0300

    Maiden tiedot initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import Country from './Country'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [countries, setCountries] = useState([]) 
+    const [ newFilter, setNewFilter ] = useState('')
+
+    /*
+    const promise = axios.get('https://restcountries.eu/rest/v2/all')
+    console.log(promise)
+    */
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => {
+                console.log('promise fulfilled')
+                setCountries(response.data)
+                console.log(response.data)
+            })
+    }, [])
+     const rows = () => countries.map(country =>
+        <Country 
+        key = {country.name}
+        country = {country}
+        />
+     )
+        
+    //const rows = () => persons.map(person => <li key = {person.name} >{person.name} {person.number}</li>)
+  
+      
+      //.map(person => <li key = {person.name} >{person.name} {person.number}</li>)
+  
+
+    const handleNoteChangeFilter = (event) =>{
+        console.log(event.target.value)
+        setNewFilter(event.target.value)
+    }
+
+    return (
+        <div>
+          <form>
+              Find countries: <input value = {newFilter}
+              onChange = {handleNoteChangeFilter}
+              />
+          </form>
+          {rows()}
+        </div>
+      )
+
+
 }
 
-export default App;
+
+
+
+
+export default App
